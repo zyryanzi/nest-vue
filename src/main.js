42429c640aac6d5d60386d4cc71ec6d6c0a31071
uray <zyryanzi@163.com>
2021-01-04 23:47:53 +0800

    'App'

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import Cookies from 'js-cookie'
+import Element from 'element-ui'
+
+import mavonEditor from 'mavon-editor'
+
+const app = createApp(App)
+app.use(Element, {
+    size: Cookies.get('size') || 'small' // set element-ui default size
+})
+app.use(mavonEditor)
 createApp(App).mount('#app')
